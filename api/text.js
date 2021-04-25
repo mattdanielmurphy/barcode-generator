6c238a2dd7a2161da913f0ffcb7fc69469b62358
Matthew Murphy <matt@mattmurphy.ca>
2021-04-24 19:55:30 -0600

    update api

--- api/text.js
+++ api/text.js
@@ -2,19 +2,7 @@ import Gitrows from 'gitrows'
 
 const gitrows = new Gitrows()
 
-module.exports = async (request, resolve) => {
-	console.log('request received')
-	resolve.status(200)
-	if (request.body && request.body.scanned) {
-		console.log('body.scanned found')
-		gitrows
-			.replace(
-				'@github/mattdanielmurphy/barcode-generator/text.json',
-				request.body,
-			)
-			.then((result) => {
-				console.log(result)
-				resolve.status('ok')
-			})
-	} else resolve.status('ok')
+module.exports = async (req, res) => {
+	const { body } = req
+	res.send(`Hello ${body.name}, you just parsed the request body!`)
 }
