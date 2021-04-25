0586b48334d9487e0dad85f9729e3bf7a5713485
Matthew Murphy <matt@mattmurphy.ca>
2021-04-24 20:18:26 -0600

    update api

--- api/text.mjs
+++ api/text.mjs
@@ -2,8 +2,14 @@ import Gitrows from 'gitrows'
 
 const gitrows = new Gitrows()
 
+const options = {
+	username: 'mattdanielmurphy',
+}
+
 module.exports = async (req, res) => {
 	const { body } = req
+	options.token = body.token
+	gitrows.options = options
 	const data = { scanned: body.scanned }
 	console.log(data)
 	await gitrows.put(
