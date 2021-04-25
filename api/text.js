94f257e84976056425057e4ea7151ea1df6f8c04
Matthew Murphy <matt@mattmurphy.ca>
2021-04-24 21:42:56 -0600

    try to fix problem

--- api/text.js
+++ api/text.js
@@ -1,26 +1,19 @@
 import Gitrows from 'gitrows'
 
-const gitrows = new Gitrows()
-
-const options = {
-	username: 'mattdanielmurphy',
-}
-
 module.exports = async (req, res) => {
 	const { body } = req
-	options.token = body.token
-	gitrows.options(options)
+	const token = body.token
+	const gitrows = new Gitrows({ username: 'mattdanielmurphy', token })
 	const data = { scanned: body.scanned }
-	console.log(data, body.token)
 	await gitrows
 		.replace('@github/mattdanielmurphy/temp/text.json', data)
 		.then((v) => {
 			console.log('posted')
 			console.log(v)
-			res.send('Good!')
 		})
 		.catch((err) => {
 			console.log('ERROR', err)
 			res.send(err)
 		})
+	res.send('Good!')
 }
