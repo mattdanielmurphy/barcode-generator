45f5a58ff29a456e6af98bda8072cb7bafae0e11
Matthew Murphy <matt@mattmurphy.ca>
2021-04-27 17:49:03 -0600

    update api

--- api/text.js
+++ api/text.js
@@ -2,12 +2,28 @@ import faunadb from 'faunadb'
 require('dotenv').config()
 const q = faunadb.query
 const client = new faunadb.Client({ secret: process.env.DB_SECRET })
-module.exports = async (req, res) => {
-	const text = await client
+
+async function handleGET(res) {
+	const { data } = await client
 		.query(
 			q.Get(q.Ref(q.Collection('barcode-generator'), '297066273465958925')),
 		)
-		.then((result) => result.data)
 		.catch((err) => console.error('Error: %s', err))
-	res.status(200).json({ text })
+	res.status(200).json(data)
+}
+
+async function handlePOST(req, res) {
+	const object = { text: req.body.text }
+	const ref = q.Ref(q.Collection('barcode-generator'), '297066273465958925')
+
+	const data = await client
+		.query(q.Replace(ref, object))
+		.catch((err) => console.error('Error: %s', err))
+	res.status(200).json(data)
+}
+
+module.exports = async (req, res) => {
+	if (req.body.text) {
+		handlePOST(req, res)
+	} else handleGET(res)
 }
