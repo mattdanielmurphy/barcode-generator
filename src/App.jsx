2e9e1f49c2e46a461507273a4ea0431928627016
Matthew Murphy <matt@mattmurphy.ca>
2021-04-27 21:55:14 -0600

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -83,24 +83,24 @@ function App() {
 	const [UPCs, setUPCs] = useState([])
 
 	async function getTextFromDatabase() {
-		// const { data } = await axios.get(
-		// 	'https://barcode-generator-beta.vercel.app/api/text',
-		// )
-		const data = {
-			text: `068258002405
-		681131911955
-		67495900008
-		681131911962
-		67495900009
-		67495900010
-		67495900006
-		67495900002
-		67495900003
-		67495900012
-		68258618422
-		068258618309
-		067495900022`,
-		}
+		const { data } = await axios.get(
+			'https://barcode-generator-beta.vercel.app/api/text',
+		)
+		// const data = {
+		// 	text: `068258002405
+		// 681131911955
+		// 67495900008
+		// 681131911962
+		// 67495900009
+		// 67495900010
+		// 67495900006
+		// 67495900002
+		// 67495900003
+		// 67495900012
+		// 68258618422
+		// 068258618309
+		// 067495900022`,
+		// }
 		return data.text
 	}
 
@@ -109,9 +109,21 @@ function App() {
 			const text = await getTextFromDatabase()
 			setText(text)
 			function filterScannedText() {
-				// const re1 = /[\d]{0,3}[\w]{0,2}[\d]{7,}/g
-				const re1 = /[1-9]\d{10}/g
-				const matches = text.match(re1)
+				const elevenTo12Digits = /[\d\w]{11,12}/g
+				const matches = text
+					.match(elevenTo12Digits)
+					.map((match) => {
+						if (/^\d.$/.test(match)) return match
+						else return replaceLettersWithLikelyDigits(match)
+					})
+					.map((match) => {
+						console.log('mat', match)
+						const newMatch = match.replace(/^0/, '')
+						console.log(newMatch)
+						return /^\d{11}/.exec(newMatch)[0]
+					})
+				// const re1 = /[1-9]\d{10}/g
+				// const matches = text.match(re1)
 				return matches
 			}
 			setUPCs(filterScannedText())
@@ -156,43 +168,43 @@ const Container = styled.div`
 	margin: 3em auto;
 `
 
-// const letterToNumber = {
-// 	A: 8,
-// 	B: 8,
-// 	C: 0,
-// 	D: 0,
-// 	E: 8,
-// 	F: 8,
-// 	G: 0,
-// 	H: 8,
-// 	I: 1,
-// 	J: 0,
-// 	K: 8,
-// 	L: 1,
-// 	M: 0,
-// 	N: 8,
-// 	O: 0,
-// 	P: 2,
-// 	Q: 0,
-// 	R: 8,
-// 	S: 5,
-// 	T: 7,
-// 	U: 0,
-// 	V: 0,
-// 	W: 0,
-// 	X: 0,
-// 	Y: 1,
-// 	Z: 0,
-// }
+const letterToNumber = {
+	A: 8,
+	B: 8,
+	C: 0,
+	D: 0,
+	E: 8,
+	F: 8,
+	G: 0,
+	H: 8,
+	I: 1,
+	J: 0,
+	K: 8,
+	L: 1,
+	M: 0,
+	N: 8,
+	O: 0,
+	P: 2,
+	Q: 0,
+	R: 8,
+	S: 5,
+	T: 7,
+	U: 0,
+	V: 0,
+	W: 0,
+	X: 0,
+	Y: 1,
+	Z: 0,
+}
 
-// function replaceLettersWithLikelyDigits(str) {
-// 	return str
-// 		.split('')
-// 		.map((digit) => {
-// 			if (isNaN(Number(digit))) {
-// 				console.log(digit)
-// 				return letterToNumber[digit]
-// 			} else return digit
-// 		})
-// 		.join('')
-// }
+function replaceLettersWithLikelyDigits(str) {
+	return str
+		.split('')
+		.map((digit) => {
+			if (isNaN(Number(digit))) {
+				console.log(digit)
+				return letterToNumber[digit]
+			} else return digit
+		})
+		.join('')
+}
