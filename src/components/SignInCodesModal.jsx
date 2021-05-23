f9cdebe93a6c8f5d8cc66d58ec45862486e29b66
Matthew Murphy <matt@mattmurphy.ca>
2021-05-22 22:57:01 -0600

    add simple auth

--- src/components/SignInCodesModal.jsx
+++ src/components/SignInCodesModal.jsx
@@ -1,22 +1,38 @@
 import { BarcodeViewer } from '.'
+import Cookies from 'js-cookie'
 import { Modal } from 'antd'
+import { useState } from 'react'
 
-const SignInCodesViewer = () => (
-	<BarcodeViewer
-		noWrapper
-		UPCs={[process.env.REACT_APP_USER, process.env.REACT_APP_PASS]}
-	/>
-)
-
-const modalOptions = {
-	icon: false,
-	content: <SignInCodesViewer />,
-	onOk() {},
-	maskClosable: true,
-	okButtonProps: {
-		size: 'large',
-	},
-	centered: true,
+const SignInCodesViewer = ({ authenticated }) => {
+	const [auth, setAuth] = useState(authenticated)
+	return auth ? (
+		<BarcodeViewer
+			noWrapper
+			UPCs={[process.env.REACT_APP_USER, process.env.REACT_APP_PASS]}
+		/>
+	) : (
+		<input
+			type='password'
+			onChange={(e) => {
+				if (e.target.value === 'houseplants') {
+					Cookies.set('user', 'houseplants')
+					setAuth(true)
+				}
+			}}
+		/>
+	)
 }
 
-export const SignInCodesModal = () => Modal.info(modalOptions)
+export const SignInCodesModal = () => {
+	const user = Cookies.get('user')
+	Modal.info({
+		icon: false,
+		content: <SignInCodesViewer authenticated={user === 'houseplants'} />,
+		onOk() {},
+		maskClosable: true,
+		okButtonProps: {
+			size: 'large',
+		},
+		centered: true,
+	})
+}
